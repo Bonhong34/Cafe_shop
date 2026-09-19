da5156654efbf83a0296b7834e33e58d4034ded2
Bonhong34 <hornbonhong@gmail.com>
2026-09-19 14:38:54 +0700

    Use secure_asset for custom CSS link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('css/custom.css') }}">
     @stack('styles')
     <!-- <link href="{{ asset('css/shop.css') }}" rel="stylesheet"> -->
 </head>
